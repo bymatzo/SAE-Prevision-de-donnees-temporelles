397527c7195c7036eb9bcd66fa44b429a9e612bc
bymatzo <bymatzo667@gmail.com>
2026-03-08 14:16:00 +0100

    jsp

--- Analyse.R
+++ Analyse.R
@@ -1,10 +1,21 @@
 library(readr)
 
-url_train <- "https://raw.githubusercontent.com/bymatzo/SAE-Prevision-de-donnees-temporelles/data/Données_Ponctualité_Train_Brut.csv"
+url_train <- "https://raw.githubusercontent.com/bymatzo/SAE-Prevision-de-donnees-temporelles/data/Données_Ponctualité_Train_Corrigées.csv"
 url_netflix <- "https://raw.githubusercontent.com/bymatzo/SAE-Prevision-de-donnees-temporelles/data/Données_Clients_Netflix.csv"
 
-train <- read_csv(url_train)
-netflix <- read_csv(url_netflix)
+train <- read_delim(
+  file = url_train,
+  delim = ";",
+  locale = locale(encoding = "latin1")
+)
+
+netflix <- read_delim(
+  file = url_netflix,
+  delim = ";",
+  locale = locale(encoding = "UTF-8")
+)
 
 head(train)
 head(netflix)
+
+
